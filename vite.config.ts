40f95f4de38caa7a61187741f4a4b29a78e76952
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2025-06-28 18:24:59 +0530

    Added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/free-read/',
+  // base: '/', // <- IMPORTANT for custom domains
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
